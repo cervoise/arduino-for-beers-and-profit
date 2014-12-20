7b02cae376a1c66a0c830d142371dd63f7f7e5f4
Julien Reitzel <julien@reitzel.info>
2014-12-20 18:20:34 +0100

    Add a RTC mdule to log date/time

--- temperature-watcher/temperator.ino
+++ temperature-watcher/temperator.ino
@@ -1,32 +1,44 @@
 /*
-  Needs Arduino Uno, Ethernet Shield (for SD card) and BMP180 from Sparkfun.
-  Temperature mesure every DELAY and write it on SD card (needs FAT16).
+  Needs Arduino Uno, Ethernet Shield (for SD card), BMP180 from Sparkfun, and a RTC module (DS1338 for example).
+  Temperature mesure every DELAY and write it on SD card (needs FAT16) in CSV format (standard LibreOffice Calc format with UK locale so you can create a chart immediately).
+  First column is the date and time of the measurement with following format: "DD/MM/YYYY HH:MM:SS". 
+  Second column is the temperature.
+  
+  BMP180 and RTC are I²C devices and will both be connected to SDA and SCL pins, but take care that only one set of pull-up resistors is enabled.
 */
 
 #include <SFE_BMP180.h>
 #include <Wire.h>
 #include <SD.h>
+#include <Time.h>
+#include <DS1307RTC.h>
 
 File tempTxtFile;
 SFE_BMP180 pressure;
 
+//On pin 13 a led is connected on the Arduino board but is hidden by the Ethernet shield. You can connect an external led if you wish.
+#define ERROR_LED 13
 //in meters
 #define ALTITUDE 30.0
 //in millisecond
 #define DELAY 5000
+//CSV file separator
+#define CSV_SEPARATOR ","
 
 void setup()
 {
+  pinMode(ERROR_LED, OUTPUT);
+  
   Serial.begin(9600);
   Serial.println("REBOOT");
 
   // Initialize the sensor (it is important to get calibration values stored on the device).
-  if (pressure.begin())
+  if (pressure.begin()) {
     Serial.println("BMP init success.");
-  else
-  {
+  }
+  else {
     Serial.println("BMP init fail.");
-    return;
+    fatalError();
   }
   
   //init the SD card
@@ -34,10 +46,10 @@ void setup()
    
   if (!SD.begin(4)) {
     Serial.println("SD initialization failed.");
-    return;
+    fatalError();
   }
+  
   Serial.println("SD initialization done.");
- 
 }
 
 void loop()
@@ -45,6 +57,7 @@ void loop()
   
   char status;
   double T;
+  tmElements_t tm;
   
   // Start a temperature measurement:
   // If request is successful, the number of ms to wait is returned.
@@ -63,33 +76,69 @@ void loop()
     status = pressure.getTemperature(T);
     if (status != 0)
     {
-      // Print out the measurement:
-      Serial.print("temperature: ");
-      Serial.print(T,2);
-      Serial.println(" deg C.");
-      
-      //write on SD card
-      // open the file. note that only one file can be open at a time,
-      // so you have to close this one before opening another.
-        tempTxtFile = SD.open("temp.txt", FILE_WRITE);
-     
-      // if the file opened okay, write to it:
-      if (tempTxtFile) {
-        Serial.print("Writing to temp.txt...");
-        tempTxtFile.println(T,2);
-        // close the file:
-        tempTxtFile.close();
-        Serial.println("done.");
-      } else {
-        // if the file didn't open, print an error:
-        Serial.println("error opening temp.txt");
+      if (RTC.read(tm)) {
+        
+        // date/time format is : DD/MM/YYYY HH:MM:SS
+        String time = String(tm.Day) + "/" + String(tm.Month) + "/" + tmYearToCalendar(tm.Year) + " " + getTwoDigitsNumber(tm.Hour) + ":" + getTwoDigitsNumber(tm.Minute) + ":" + getTwoDigitsNumber(tm.Second);
+        
+        // Print out the measurement:
+        Serial.print("At ");
+        Serial.print(time);
+        Serial.print(" - temperature: ");
+        Serial.print(T,2);
+        Serial.println(" deg C.");
+        
+        // Write on SD card. Open and close the file for each measurement.
+        tempTxtFile = SD.open("temp.csv", FILE_WRITE);
+       
+        // if the file opened okay, write to it:
+        if (tempTxtFile) {
+          Serial.print("Writing to temp.csv...");
+          tempTxtFile.print(time);
+          tempTxtFile.print(CSV_SEPARATOR);
+          tempTxtFile.println(T,2);
+          // close the file:
+          tempTxtFile.close();
+          Serial.println("done.");
+          
+        }
+        else {
+          // if the file didn't open, print an error:
+          Serial.println("error opening temp.csv");
+          fatalError();
+        }
+      } 
+      else {
+        Serial.println("DS1307 read error!  Please check the circuitry.");
+        fatalError();
       }
-     
     }
-
-    else Serial.println("error retrieving temperature measurement\n");
+    else {
+      Serial.println("error retrieving temperature measurement\n");
+    }
   }
-  else Serial.println("error starting temperature measurement\n");
-
+  else {
+    Serial.println("error starting temperature measurement\n");
+  }
+  
   delay(DELAY);
+}
+
+
+void fatalError() {
+  while(true) {
+    digitalWrite(ERROR_LED, HIGH);
+    delay(300);
+    digitalWrite(ERROR_LED, LOW);
+    delay(300);
+  }
+}
+
+String getTwoDigitsNumber(int number) {
+  if (number >= 0 && number < 10) {
+    return "0" + String(number);
+  }
+  else {
+    return String(number);
+  }
 }
